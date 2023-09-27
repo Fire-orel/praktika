9a630565f89b0b1f685dd472e84d7645d27157fb
Fire-orel <78948895+Fire-orel@users.noreply.github.com>
2023-09-28 01:16:23 +0900

    28.09.2023 1:16

--- praktika/edit_grup.Designer.cs
+++ praktika/edit_grup.Designer.cs
@@ -85,7 +85,7 @@
             this.Controls.Add(this.name_grup);
             this.Controls.Add(this.label1);
             this.Name = "edit_grup";
-            this.Text = "edit_grup";
+            this.Text = "Изменения группы";
             this.Load += new System.EventHandler(this.edit_grup_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
